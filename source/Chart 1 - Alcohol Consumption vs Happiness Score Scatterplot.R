a048aabc1e31d2ccc80cf8538cb13b890c30f634
protovisor <lapikerr@uw.edu>
2022-11-14 19:51:14 -0800

    this is the first chart, a scatterplot comparing alcohol consumption vs happiness score

---
+++ source/Chart 1 - Alcohol Consumption vs Happiness Score Scatterplot.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+#you gotta load the rdata from the specific rworkspace file within the data folder
+
+alcohol_vs_happiness <- HappinessAlcoholConsumption %>%
+  group_by(Country, HappinessScore, GDP_PerCapita) %>%
+  summarize("total_alcohol_consumption" = sum(Beer_PerCapita, Spirit_PerCapita, Wine_PerCapita, na.rm = TRUE)) 
+
+GDP_vs_alcohol_consumption_scatterplot <- ggplot(alcohol_vs_happiness, aes(x = total_alcohol_consumption, y = GDP_PerCapita)) + geom_point(shape=1)
+
+GDP_vs_alcohol_consumption_scatterplot+ labs(x = 'Total Alcohol Consumption Per Capita (Beer, Spirits, Wine)', y = 'GDP Per Capita', title = 'Alcohol Consumption By Country vs GDP')
+
+alcohol_vs_happiness_scatterplot <- ggplot(alcohol_vs_happiness, aes(x = total_alcohol_consumption, y = HappinessScore)) + geom_point(shape=1)
+  
+alcohol_vs_happiness_scatterplot+ labs(x = 'Total Alcohol Consumption Per Capita (Beer, Spirits, Wine)', y = 'Happiness Score', title = 'Alcohol Consumption By Country vs Happiness Score')
+
+#There's no easy way to say this, but there's absolutely no relation between happiness and alcohol consumption. With a scatter plot it's easiest to see; you can say that there's a slow rising trend in happiness, but considering there are many other factors such as GDP per capita and ability to actually buy alcohol there's absolutely no relation between alcohol consumption and happiness score. The happiest countries are those with the most money, and thereby the ability to afford more alcohol to consume. 
